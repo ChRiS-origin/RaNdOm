96e24da06fb1bba843c5dda515802e02f8c128e2
ChRiS-origin <Wahlicht@vodafonemail.de>
2024-08-03 00:43:00 +0200

    Test the nesting and added a ModeSwitch

---
+++ src/lib/components/ModeSwitch.svelte
@@ -0,0 +1,26 @@
+<script>
+    let RandMode = 'R@ŊðØm';
+
+    const handleClick = () => {
+        if (RandMode === 'R@ŊðØm') {
+            RandMode = 'R@ŊðØm-list';
+        } else {
+            RandMode = 'R@ŊðØm';
+        }
+    };
+</script>
+
+<main>
+    <button class="modeButton" on:click={handleClick}>{RandMode}</button>
+</main>
+
+
+<style>
+	.modeButton{
+		color:red;
+		font-size:1em;
+        border: none;
+        background-color: transparent;
+		padding:0.1em;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import ModeSwitch from '$lib/components/ModeSwitch.svelte';
+</script>
+
+<ModeSwitch/>
+
+<style>
+        main {
+            text-align: center;
+            background-color: black;
+            padding: 1em;
+            max-width: 240px;
+            margin: 0 auto;
+        }
+
+        p {
+            color: #ff3e00;
+            text-transform: uppercase;
+            font-size: 4em;
+            font-weight: 100;
+        }
+
+        @media (min-width: 640px) {
+            main {
+                max-width: none;
+            }
+        }
+</style>
