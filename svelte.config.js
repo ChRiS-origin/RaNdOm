ed9fabdf62b7305c7cdeb25231a93a50ebbf4562
ChRiS-origin <Wahlicht@vodafonemail.de>
2024-08-17 23:50:53 +0200

    deploy?

--- svelte.config.js
+++ svelte.config.js
@@ -25,10 +25,6 @@ const config = {
 		fallback: null,
 		precompress: false
 		}),
-		prerender: {
-			// This can be false if you're using a fallback (i.e. SPA mode)
-			default: true
-		}
 	},
 	  preprocess: vitePreprocess()
 };
