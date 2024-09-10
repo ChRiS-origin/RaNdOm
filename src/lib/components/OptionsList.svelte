ea01b3ceb2fc449f00e118c551de0a2b841be642
ChRiS-origin <Wahlicht@vodafonemail.de>
2024-09-10 17:46:38 +0200

    fix color of << for light-mode

--- src/lib/components/OptionsList.svelte
+++ src/lib/components/OptionsList.svelte
@@ -60,7 +60,7 @@
                     <Option option={option} optionIndex={optionIndex}/>
                 {:else}
                     <li class="nothingThere">
-                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m296-224-56-56 240-240 240 240-56 56-184-183-184 183Zm0-240-56-56 240-240 240 240-56 56-184-183-184 183Z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f5d5d"><path d="m296-224-56-56 240-240 240 240-56 56-184-183-184 183Zm0-240-56-56 240-240 240 240-56 56-184-183-184 183Z"/></svg>
                     </li>
                 {/each}
             </ul>
